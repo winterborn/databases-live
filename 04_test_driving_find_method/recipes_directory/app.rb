b485d6bf2e7271ab8484501263ee682247a2c65a
Philip White <phwhite89@gmail.com>
2022-08-24 10:57:53 +0200

    added find method to app.rb for finding recipe by id.

--- 04_test_driving_find_method/recipes_directory/app.rb
+++ 04_test_driving_find_method/recipes_directory/app.rb
@@ -6,12 +6,13 @@ require_relative "lib/recipe_repository"
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('recipes_directory')
 
-# Print out each record from the result set:
+# Print out each record from the result set for all:
 recipe_repository = RecipeRepository.new
 recipe_repository.all.each { |recipe| p "#{recipe.names}: #{recipe.cooking_time} mins - #{recipe.rating} stars in difficulty" }
 
-
-
+# Print out a single record from the result set for find:
+recipe = recipe_repository.find(3)
+puts "#{recipe.names}: #{recipe.cooking_time} mins - #{recipe.rating} stars in difficulty"
 
 
 
