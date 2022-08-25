57afcb43425e7324e0412c3a660a3cfd21999239
Philip White <phwhite89@gmail.com>
2022-08-25 18:51:48 +0200

    new directory.

---
+++ phase_02_challenges/02_test_driving_model_repository_classes/book_store/lib/book.rb
@@ -0,0 +1,4 @@
+class Book
+    # Replace the attributes by your own columns.
+    attr_accessor :id, :title, :author_name
+  end
---
+++ phase_02_challenges/02_test_driving_model_repository_classes/book_store/spec/seeds_books.sql
@@ -0,0 +1,13 @@
+-- Write your SQL seed here.
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+TRUNCATE TABLE books RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO books (title, author_name) VALUES ('Ready Player One', 'Ernest Cline');
+INSERT INTO books (title, author_name) VALUES ('Artemis', 'Andy Weir');
